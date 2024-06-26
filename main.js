3cd8c143a12054ec368724787d749af2b3d0f967
rlazar25 <119755392+rlazar25@users.noreply.github.com>
2024-06-26 14:04:46 +0200

    Syntax for oop

---
+++ main.js
@@ -0,0 +1,37 @@
+/* let now = new Date();
+now.getFullYear(); 
+console.log(now); */
+
+// object fabric
+// function user (a, b){
+//     let u = {};
+//     u.group = "Student"
+//     u.name = a;
+//     u.lastName = b;
+//     u.info = function(){
+//         console.log(this.group);
+//     }
+//     return u;
+// }
+
+// let u1 = user("Danilo", "Vesovic");
+// // let u2 = user("Ljubica", "Miric");
+// // console.log(u1);
+// // console.log(u2);
+// u1.info();
+// console.log(u1 instanceof user);
+
+
+// syntax for oop
+// function User(a, b){
+//     // let this = {}
+//     this.name = a;
+//     this.lastName = b;
+//     // return this
+// }
+//
+// let u1 = new User("Danilo", "Vesovic");
+// let u2 = new User("Ivan", "Vesovic");
+// console.log(u1);
+// console.log(u2);
+// console.log(u1 instanceof User);
